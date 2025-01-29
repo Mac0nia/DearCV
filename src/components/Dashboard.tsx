c3cbf39ef91d3270ae51b90ca19197182430cabe
Gianluca Galli <gianluca.galli9519@gmail.com>
2025-01-29 00:11:19 +0000

    dashboard number 1 complete

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import AnimatedBackground from '../components/AnimatedBackground';
 import './Dashboard.css';
-import { FileText, CheckCircle, AlertCircle, FileUp, Link, Eye } from 'lucide-react';
+import { FileText, CheckCircle, AlertCircle, FileUp, Link, Briefcase, Upload, Eye } from 'lucide-react';
 
 type Resume = { 
   id: number; 
@@ -37,35 +37,32 @@ const Dashboard: React.FC = () => {
               <div className="flex items-center justify-center w-12 h-12 bg-primary/10 rounded-lg mb-4">
                 <FileUp className="h-6 w-6 text-primary"/>
               </div>
-              <h3 className="text-xl font-bold text-white mb-2">Create New Optimized Resume</h3>
-              <button className="bg-primary hover:bg-primary-hover text-white w-full px-4 py-2 rounded-lg transition-colors duration-200">Create Resume</button>
+              <h3 className="text-xl font-bold text-white mb-2">Upload CV</h3>
+              <button className="bg-primary hover:bg-primary-hover text-white w-full px-4 py-2 rounded-lg transition-colors duration-200">Upload</button>
             </div>
           </div>
           <div className="group relative upload-job-description"> 
             <div className="relative bg-white/5 backdrop-blur-sm rounded-xl p-4 ring-1 ring-white/10 hover:ring-white/20 transition-all h-full flex flex-col">
               <div className="flex items-center justify-center w-12 h-12 bg-primary/10 rounded-lg mb-4">
-                <Link className="h-6 w-6 text-primary" />
+                <Briefcase className="h-6 w-6 text-primary"/>
               </div>
               <h3 className="text-xl font-bold text-white mb-2">Upload Job</h3>
-              <input type="url" placeholder="Enter Job URL" className="upload-url text-white p-2 rounded-lg mb-2 w-full" />
-              <button className="bg-primary hover:bg-primary-hover text-white w-full px-4 py-2 rounded-lg transition-colors duration-200">Upload</button>
+              <input type="url" placeholder="Enter Job URL" className="upload-url text-white px-4 py-2 rounded-lg mb-2 w-full" />
             </div>
           </div>
           <div className="group relative view-resumes"> 
             <div className="relative bg-white/5 backdrop-blur-sm rounded-xl p-4 ring-1 ring-white/10 hover:ring-white/20 transition-all h-full flex flex-col">
               <div className="flex items-center justify-center w-12 h-12 bg-primary/10 rounded-lg mb-4">
-                <Eye className="h-6 w-6 text-primary" />
+                <Eye className="h-6 w-6 text-primary"/>
               </div>
-              <h3 className="text-xl font-bold text-white mb-2">View Optimised Resume</h3>
-              <ul className="text-white/70">
-                {resumes.map((resume, index) => (
-                  <li key={index} className="mb-2">
-                    <button onClick={() => handleViewResume(resume.id.toString())} className="bg-primary hover:bg-primary-hover text-white px-4 py-2 rounded-lg transition-colors duration-200">View Resume {index + 1}</button>
-                  </li>
-                ))}
-              </ul>
+              <h3 className="text-xl font-bold text-white mb-2">Download CV</h3>
+              <button className="bg-primary hover:bg-primary-hover text-white w-full px-4 py-2 rounded-lg transition-colors duration-200">Download</button>
             </div>
           </div>
+          
+          
+          
+          
         </div>
       </main>
       <footer className="dashboard-footer">
